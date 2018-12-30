731ef7ad504c355747b2fc61a6afbb47f49c6494
miao shan <minnieshan@gmail.com>
2018-12-30 19:46:38 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,21 +1,23 @@
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+console.log("I just met you...");
+ console.log("and this is crazy..");
+ console.log("but here's my number..");
+ console.log("so call me");
+  return lyric;
+
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
+
+
 
 function sayMyName() {
   // fix the code in here:
@@ -27,4 +29,4 @@ function sayMyName() {
     console.log(name);
     var name = "Kristin";
   }
-}
+}*/
